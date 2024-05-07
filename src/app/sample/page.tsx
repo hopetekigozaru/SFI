3d9a3899189368c398ab2ee97586efd4135086ce
nozomu5 <you@example.com>
2024-05-07 11:22:52 +0900

    firsrCommit

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header"
+
+export default function Sample () {
+    return (
+        <div>
+            <Header/>
+            <h1>Sample</h1>
+        </div>
+    )
+}
